bf78e1db0d17a7a72e1c063afa142728c26effee
Muhammad Shafar <777uliahshafar@gmail.com>
2023-04-16 03:12:01 +0800

    articlelist

--- src/components/article-list.js
+++ src/components/article-list.js
@@ -4,55 +4,62 @@ import Article from '../components/article'
 import PaginationLinks from '../components/pagination-links.js'
 
 const IndexPage = () => {
-  const articlesPerPage = 4;
+  const articlesPerPage = 10
   let numberOfPages
   return (
-    <StaticQuery query={indexQuery} render={data => {
-      numberOfPages = Math.ceil(data.allMarkdownRemark.totalCount / articlesPerPage)
-      return (
-        <div>
-          {data.allMarkdownRemark.edges.map(({ node }) => (
-            <Article key={node.id}
-              slug={node.fields.slug}
-              keyword={node.frontmatter.keyword}
-              title={node.frontmatter.title}
-              date={node.frontmatter.date}
-              excerpt={node.excerpt}
-              fluid={node.frontmatter.image.childImageSharp.fluid}
-              tags={node.frontmatter.tags} />
-          ))}
-          <PaginationLinks currentPage={1} numberOfPages={numberOfPages} />
-        </div>
-      )
-    }} />
+    <StaticQuery
+      query={indexQuery}
+      render={data => {
+        numberOfPages = Math.ceil(
+          data.allMarkdownRemark.totalCount / articlesPerPage
+        )
+        return (
+          <div>
+            {data.allMarkdownRemark.edges.map(({ node }) => (
+              <Article
+                key={node.id}
+                slug={node.fields.slug}
+                keyword={node.frontmatter.keyword}
+                title={node.frontmatter.title}
+                date={node.frontmatter.date}
+                excerpt={node.excerpt}
+                fluid={node.frontmatter.image.childImageSharp.fluid}
+                tags={node.frontmatter.tags}
+              />
+            ))}
+            <PaginationLinks currentPage={1} numberOfPages={numberOfPages} />
+          </div>
+        )
+      }}
+    />
   )
 }
 
 const indexQuery = graphql`
   query {
-    site{
-      siteMetadata{
+    site {
+      siteMetadata {
         title
       }
     }
 
     allMarkdownRemark(
-      sort: {fields: [frontmatter___date], order: DESC}
-      limit: 4
-      ) {
+      sort: { fields: [frontmatter___date], order: DESC }
+      limit: 10
+    ) {
       totalCount
       edges {
         node {
-          fields{
+          fields {
             slug
           }
-          id 
+          id
           frontmatter {
             title
             tags
-            image{
-              childImageSharp{
-                fluid(maxWidth:750, maxHeight: 150){
+            image {
+              childImageSharp {
+                fluid(maxWidth: 750, maxHeight: 150) {
                   ...GatsbyImageSharpFluid
                 }
               }
